69c58db1be673693d10395cec38f1c328fd014f8
mobei <mobei615@foxmail.com>
2022-09-21 07:48:47 +0800

    基于AtomicLong确保多线程拉取注册表版本不错乱

--- register-client/src/main/java/com/mobei/register/client/CachedServiceRegistry.java
+++ register-client/src/main/java/com/mobei/register/client/CachedServiceRegistry.java
@@ -3,6 +3,7 @@ package com.mobei.register.client;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
@@ -13,25 +14,31 @@ import java.util.concurrent.atomic.AtomicStampedReference;
  * @remark
  */
 public class CachedServiceRegistry {
-
     /**
-     * 从注册中心拉取服务注册表的间隔周期
+     * 服务注册表拉取间隔时间
      */
     private static final Long SERVICE_REGISTRY_FETCH_INTERVAL = 30 * 1000L;
-
-    private FetchDeltaRegistryWorker fetchDeltaRegistryWorker;
-
-    /**
-     * http通信组件
-     */
-    private HttpSender httpSender;
 
     /**
      * 客户端缓存的所有的服务实例的信息
      */
-    private AtomicStampedReference<Applications> atomicStampedReferenceApplications;
-
+    private AtomicStampedReference<Applications> asrApplications;
+    /**
+     * 负责定时拉取注册表到客户端进行缓存的后台线程
+     */
+    private FetchDeltaRegistryWorker fetchDeltaRegistryWorker;
+    /**
+     * RegisterClient
+     */
     private RegisterClient registerClient;
+    /**
+     * http通信组件
+     */
+    private HttpSender httpSender;
+    /**
+     * 代表了当前的本地缓存的服务注册表的一个版本号
+     */
+    private AtomicLong applicationsVersion = new AtomicLong(0L);
 
     /**
      * 构造函数
@@ -43,7 +50,7 @@ public class CachedServiceRegistry {
         this.fetchDeltaRegistryWorker = new FetchDeltaRegistryWorker();
         this.registerClient = registerClient;
         this.httpSender = httpSender;
-        this.atomicStampedReferenceApplications = new AtomicStampedReference<>(new Applications(), 0);
+        this.asrApplications = new AtomicStampedReference<>(new Applications(), 0);
     }
 
     /**
@@ -71,20 +78,38 @@ public class CachedServiceRegistry {
     private class FetchFullRegistryWorker extends Thread {
         @Override
         public void run() {
-            // 刚刚拉取到的实例
-            Applications fetchedApplications = httpSender.fetchFullRegistry();
+            // 拉取全量注册表
+            // 这个操作要走网络，但是不知道为什么抽风了，此时就是一直卡住，数据没有返回回来
+            // 卡在这儿了，卡了几分钟
+            // 此时的这个数据是一个旧的版本，里面仅仅包含了30个服务实例
+            // 全量拉注册表的线程突然苏醒过来了，此时将30个服务实例的旧版本的数据赋值给了本地缓存注册表
+            // 将校对正确的数据更新成了错误的旧数据
+
+            // 一定要在发起网络请求之前，先拿到一个当时的版本号
+            // 接着在这里发起网络请求，此时可能会有别的线程来修改这个注册表，更新版本，在这个期间
+            // 必须是发起网络请求之后，这个注册表的版本没有被人修改过，此时他才能去修改
+            // 如果在这个期间，有人修改过注册表，版本不一样了，此时就直接if不成立，不要把你拉取到的
+            // 旧版本的注册表给设置进去
+            fetchFullRegistry();
+        }
+    }
+
+    /**
+     * 拉取全量注册表到本地
+     */
+    private void fetchFullRegistry() {
+        // version = 0
+        Long expectedVersion = applicationsVersion.get();
+        Applications fetchedApplications = httpSender.fetchFullRegistry();
+
+        // version = 1
+        if (applicationsVersion.compareAndSet(expectedVersion, expectedVersion + 1)) {
+            // 防止网络卡顿造成的旧数据去更新新的数据
             while (true) {
-                // 客户端缓存的实例
-                Applications expectedApplications = atomicStampedReferenceApplications.getReference();
-                int expectedStamp = atomicStampedReferenceApplications.getStamp();
-                /**
-                 * 用本地的AtomicReference和它里面取出来的值做CAS比较,如果引用对象是同一个,说明没有其它线程更改过,
-                 * 那么就把引用对象更新为刚刚拉取到的实例-fetchedApplications,退出循环
-                 */
-                if (atomicStampedReferenceApplications.compareAndSet(expectedApplications,
-                        fetchedApplications,
-                        expectedStamp,
-                        expectedStamp++)) {
+                Applications expectedApplications = asrApplications.getReference();
+                int expectedStamp = asrApplications.getStamp();
+                if (asrApplications.compareAndSet(expectedApplications, fetchedApplications,
+                        expectedStamp, expectedStamp + 1)) {
                     break;
                 }
             }
@@ -102,18 +127,25 @@ public class CachedServiceRegistry {
                 try {
                     Thread.sleep(SERVICE_REGISTRY_FETCH_INTERVAL);
 
-                    /**
-                     * 拉取回来的是最近3分钟变化的服务实例
-                     *
-                     * 增量注册表:一类是注册，一类是删除.如果是注册的话，就判断一下这个服务实例是否在这个本地缓存的注册表中
-                     * 如果不在的话，就放到本地缓存注册表里去.如果是删除的话，就看一下，如果服务实例存在，就给删除了
-                     */
+                    // 拉取回来的是最近3分钟变化的服务实例
+                    // 先拉了一个增量注册表，发现跟本地合并之后，条数不对
+                    Long expectedVersion = applicationsVersion.get();
                     DeltaRegistry deltaRegistry = httpSender.fetchDeltaRegistry();
 
-                    // 我们这里其实是要大量的修改本地缓存的注册表，所以此处需要加锁
-                    mergeDeltaRegistry(deltaRegistry);
-                    // 校对调整注册表
-                    reconcileRegistry(deltaRegistry);
+                    if (applicationsVersion.compareAndSet(expectedVersion, expectedVersion + 1)) {
+                        // 一类是注册，一类是删除
+                        // 如果是注册的话，就判断一下这个服务实例是否在这个本地缓存的注册表中
+                        // 如果不在的话，就放到本地缓存注册表里去
+                        // 如果是删除的话，就看一下，如果服务实例存在，就给删除了
+
+                        // 我们这里其实是要大量的修改本地缓存的注册表，所以此处需要加锁
+                        mergeDeltaRegistry(deltaRegistry);
+
+                        // 再检查一下，跟服务端的注册表的服务实例的数量相比，是否是一致的
+                        // 封装一下增量注册表的对象，也就是拉取增量注册表的时候，一方面是返回那个数据
+                        // 另外一方面，是要那个对应的register-server端的服务实例的数量
+                        reconcileRegistry(deltaRegistry);
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -126,8 +158,8 @@ public class CachedServiceRegistry {
          * @param deltaRegistry
          */
         private void mergeDeltaRegistry(DeltaRegistry deltaRegistry) {
-            synchronized (atomicStampedReferenceApplications) {
-                Map<String, Map<String, ServiceInstance>> registry = atomicStampedReferenceApplications.getReference().getRegistry();
+            synchronized (asrApplications) {
+                Map<String, Map<String, ServiceInstance>> registry = asrApplications.getReference().getRegistry();
                 LinkedList<RecentlyChangedServiceInstance> recentlyChangedServiceInstances = deltaRegistry.getRecentlyChangedQueue();
                 String serviceName, serviceInstanceId;
                 for (RecentlyChangedServiceInstance recentlyChangedItem : recentlyChangedServiceInstances) {
@@ -167,11 +199,11 @@ public class CachedServiceRegistry {
             // 封装一下增量注册表的对象，也就是拉取增量注册表的时候，一方面是返回那个数据
             // 另外一方面，是要那个对应的register-server端的服务实例的数量
             Long serverSideTotalCount = deltaRegistry.getServiceInstanceTotalCount();
-            Map<String, Map<String, ServiceInstance>> registry = atomicStampedReferenceApplications.getReference().getRegistry();
-            int expectedStamp = atomicStampedReferenceApplications.getStamp();
+            Map<String, Map<String, ServiceInstance>> registry = asrApplications.getReference().getRegistry();
+            int expectedStamp = asrApplications.getStamp();
 
             Long clientSideTotalCount = 0L;
-            for (Map<String, ServiceInstance> serviceInstanceMap : registry.values()){
+            for (Map<String, ServiceInstance> serviceInstanceMap : registry.values()) {
                 clientSideTotalCount += serviceInstanceMap.size();
             }
 
@@ -181,16 +213,16 @@ public class CachedServiceRegistry {
 
                 while (true) {
                     // 客户端缓存的实例
-                    Applications expectedApplications = atomicStampedReferenceApplications.getReference();
+                    Applications expectedApplications = asrApplications.getReference();
 
                     /**
                      * 用本地的AtomicReference和它里面取出来的值做CAS比较,如果引用对象是同一个,说明没有其它线程更改过,
                      * 那么就把引用对象更新为刚刚拉取到的实例-fetchedApplications,退出循环
                      */
-                    if (atomicStampedReferenceApplications.compareAndSet(expectedApplications,
+                    if (asrApplications.compareAndSet(expectedApplications,
                             fetchedApplications,
                             expectedStamp,
-                            expectedStamp ++)) {
+                            expectedStamp++)) {
                         break;
                     }
                 }
@@ -218,7 +250,7 @@ public class CachedServiceRegistry {
      * @return
      */
     public Map<String, Map<String, ServiceInstance>> getRegistry() {
-        return atomicStampedReferenceApplications.getReference().getRegistry();
+        return asrApplications.getReference().getRegistry();
     }
 
     /**
